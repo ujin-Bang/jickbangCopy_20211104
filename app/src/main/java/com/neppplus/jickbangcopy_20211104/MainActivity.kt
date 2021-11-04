1a05312944fd2714866e5897a6d021a9b1aadc16
ujin-Bang <qkddnwls14@gmail.com>
2021-11-04 15:33:52 +0900

    리스트뷰 이벤트 처리 진행중   -클릭된 방 객체만 추출 (position 활용)

--- app/src/main/java/com/neppplus/jickbangcopy_20211104/MainActivity.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20211104/MainActivity.kt
@@ -15,12 +15,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mRooms.add(RoomData(8000, "서울시 동대문구" , 5, "1번째방입니다."))
-        mRooms.add(RoomData(18000, "서울시 서대문구", 0, "2번째방입니다"))
+        mRooms.add(RoomData(18000, "강원도 철원군", 0, "2번째방입니다"))
         mRooms.add(RoomData(29700, "경기도 고양시", 17, "3번째방입니다"))
-        mRooms.add(RoomData(4300, "서울시 중구",2, "4번째방입니다."))
-        mRooms.add(RoomData(43300, "서울시 남구", 2, "5번째방입니다."))
+        mRooms.add(RoomData(4300, "서울시 중구",-3, "4번째방입니다."))
+        mRooms.add(RoomData(43300, "부산 동구", 2, "5번째방입니다."))
 
         mRoomAdapter = RoomAdapter(this, R.layout.room_list_item, mRooms)
         roomListView.adapter = mRoomAdapter
+
+        roomListView.setOnItemClickListener { adapterView, view, position, l ->  }
+
+            val clickedRoom = mRooms[position]
     }
 }
