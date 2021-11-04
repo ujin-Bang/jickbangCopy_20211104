90320c6ac2c99220f02f07344501e332a144d160
ujin-Bang <qkddnwls14@gmail.com>
2021-11-04 12:17:21 +0900

    부모에게 넘겨주기위한 파라미터를 어뎁터에 넘겨주기

--- app/src/main/java/com/neppplus/jickbangcopy_20211104/adapters/RoomAdapter.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20211104/adapters/RoomAdapter.kt
@@ -1,7 +1,9 @@
 package com.neppplus.jickbangcopy_20211104.adapters
 
+import android.content.Context
+import android.security.identity.AccessControlProfileId
 import android.widget.ArrayAdapter
 import com.neppplus.jickbangcopy_20211104.datas.RoomData
 
-class RoomAdapter : ArrayAdapter<RoomData>() {
+class RoomAdapter(val mContext: Context, val resId: Int, val mList: ArrayList<RoomData>) : ArrayAdapter<RoomData>() {
 }
