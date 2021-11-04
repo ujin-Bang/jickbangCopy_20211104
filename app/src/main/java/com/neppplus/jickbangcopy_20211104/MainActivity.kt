569dd8ee23902a31ba8e1c83b1f7c344e6c6c9ba
ujin-Bang <qkddnwls14@gmail.com>
2021-11-04 12:44:45 +0900

    어댑터 변수 객체화 +리스트뷰의 어댑터로 연결

--- app/src/main/java/com/neppplus/jickbangcopy_20211104/MainActivity.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20211104/MainActivity.kt
@@ -2,19 +2,25 @@ package com.neppplus.jickbangcopy_20211104
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.jickbangcopy_20211104.adapters.RoomAdapter
 import com.neppplus.jickbangcopy_20211104.datas.RoomData
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     val mRooms = ArrayList<RoomData>()
+    lateinit var mRoomAdapter: RoomData
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mRooms.add(RoomData(8000, "서울시 동대문구 , 5, 1번째방입니다."))
-        mRooms.add(RoomData(18000,"서울시 서대문구", 0,"2번째방입니다"))
-        mRooms.add(RoomData(29700,"경기도 고양시", 17,"3번째방입니다"))
-        mRooms.add(RoomData(4300,"서울시 중구,2,""4번째방입니다."))
-        mRooms.add(RoomData(43300,"서울시 남구,2,""5번째방입니다."))
+        mRooms.add(RoomData(18000, "서울시 서대문구", 0, "2번째방입니다"))
+        mRooms.add(RoomData(29700, "경기도 고양시", 17, "3번째방입니다"))
+        mRooms.add(RoomData(4300, "서울시 중구,"2, "4번째방입니다."))
+        mRooms.add(RoomData(43300, "서울시 남구", 2, "5번째방입니다."))
 
+        mRoomAdapter = RoomAdapter(this, R.layout.room_list_item, mRooms)
+        roomListView.adapter = mRoomAdapter
+    }
 }
