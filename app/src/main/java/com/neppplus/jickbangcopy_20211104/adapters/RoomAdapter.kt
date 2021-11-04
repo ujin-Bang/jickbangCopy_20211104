609887efeef5233d68847281d49ffe958fa34b3e
ujin-Bang <qkddnwls14@gmail.com>
2021-11-04 12:18:19 +0900

    부모 생성자의 요구사항 해결

--- app/src/main/java/com/neppplus/jickbangcopy_20211104/adapters/RoomAdapter.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20211104/adapters/RoomAdapter.kt
@@ -5,5 +5,7 @@ import android.security.identity.AccessControlProfileId
 import android.widget.ArrayAdapter
 import com.neppplus.jickbangcopy_20211104.datas.RoomData
 
-class RoomAdapter(val mContext: Context, val resId: Int, val mList: ArrayList<RoomData>) : ArrayAdapter<RoomData>() {
+class RoomAdapter(val mContext: Context,
+                  val resId: Int,
+                  val mList: ArrayList<RoomData>) : ArrayAdapter<RoomData>(mContext,resId,mList) {
 }
